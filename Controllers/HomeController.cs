f12d582c0e5b0b941e4d8de30e339b9a71d00ed9
namvgt <namvgt75@gmail.com>
2021-05-05 20:08:50 +0700

    first commit
    adding template

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LTTH_NhaHang.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+        public ActionResult Blog()
+        {
+
+            return View();
+        }
+    }
+}
